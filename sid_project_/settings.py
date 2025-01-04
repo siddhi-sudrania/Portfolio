f191d64931c1af31bad8af94223f21176523c1eb
Siddhi Sudrania <siddhisudrania09@gmail.com>
2025-01-04 14:57:01 +0530

    Update settings.py

--- sid_project_/settings.py
+++ sid_project_/settings.py
@@ -10,7 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
-from pathlib import Path,os
+from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-ed)o906gpu&^#$jzfuhxh$)tbb9w6rg148-fa(te#tdpb-jk0r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
